3b9f0cd34962a9fff3087562bb27893015f3f0a1
gogoasealin <gogoasealin@gmail.com>
2018-11-01 19:18:03 +0200

    Fixed : More than one word make programes crash because of space.

--- Tema2Caesar/Tema2Caesar/Main.c
+++ Tema2Caesar/Tema2Caesar/Main.c
@@ -16,81 +16,86 @@ int main()
 		scanf_s("%d", &option);
 		switch (option)
 		{
-			case 1:
-				printf("Introduceti mesajul decriptat: ");
-				scanf_s("%s", &mesaj, sizeof(mesaj));
-				printf("Introduceti cheia de deplasare(pozitiva): ");
-				scanf_s("%d", &key);
-				for (i = 0; i < strlen(mesaj); ++i)
+		case 1:
+			printf("Introduceti mesajul decriptat: ");
+			scanf_s(" %[^'\n']s", mesaj, sizeof(mesaj));
+			printf("%s", mesaj);
+			printf("Introduceti cheia de deplasare(pozitiva): ");
+			scanf_s("%d", &key);
+			for (i = 0; i < strlen(mesaj); ++i)
+			{
+				if (isupper(mesaj[i]))
 				{
-					if (isupper(mesaj[i]))
-					{
-						if (mesaj[i] + key < 'Z') {
-							printf("%c", mesaj[i] + key);
-						}
-						else if (mesaj[i] + key > 'Z') {
-							printf("%c", (mesaj[i] + key) - 26);
-						}
+					if (mesaj[i] + key < 'Z') {
+						printf("%c", mesaj[i] + key);
 					}
-					else {
-						if (isdigit(mesaj[i]))
-						{
-							if ((mesaj[i] + key) <= '9')
-							{
-								printf("%c", mesaj[i] + key);
-							}
-							else {
-								printf("%c", (mesaj[i] + key) - 10);
-							}
-						}
-						else if (mesaj[i] + key < 'z') {
-							printf("%c", mesaj[i] + key);
-						}
-						else if (mesaj[i] + key > 'z') {
-							printf("%c", (mesaj[i] + key) - 26);
-						}
+					else if (mesaj[i] + key > 'Z') {
+						printf("%c", (mesaj[i] + key) - 26);
 					}
 				}
-				break;
-			case 2:
-				printf("Introduceti mesajul criptat: ");
-				scanf_s("%s", &mesaj, sizeof(mesaj));
-				printf("Introduceti cheia de deplasare: ");
-				scanf_s("%d", &key);
-				for (i = 0; i < strlen(mesaj); ++i)
-				{
-					if (isupper(mesaj[i]))
+				else {
+					if (isdigit(mesaj[i]))
 					{
-						if (mesaj[i] + key < 'A') {
-							printf("%c", mesaj[i] - key);
+						if ((mesaj[i] + key) <= '9')
+						{
+							printf("%c", mesaj[i] + key);
 						}
-						else if (mesaj[i] + key > 'A') {
-							printf("%c", (mesaj[i] - key) + 26);
+						else {
+							printf("%c", (mesaj[i] + key) - 10);
 						}
 					}
-					else {
-						if (isdigit(mesaj[i]))
-						{
-							if ((mesaj[i] - key) >= '0')
-							{
-								printf("%c", mesaj[i] - key);
-							}
-							else {
-								printf("%c", (mesaj[i] - key) + 10);
-							}
-						}
-						else if (mesaj[i] + key > 'a') {
-							printf("%c", mesaj[i] - key);
-						}
-						else if (mesaj[i] + key < 'a') {
-							printf("%c", (mesaj[i] - key) + 26);
-						}
+					else if (mesaj[i] + key < 'z') {
+						printf("%c", mesaj[i] + key);
+					}
+					else if (mesaj[i] + key > 'z') {
+						printf("%c", (mesaj[i] + key) - 26);
 					}
 				}
-				break;
-			default:
-				printf("Va rugam alegeti una dintre optiunile afisate");
-				break;
+			}
+			break;
+		case 2:
+			printf("Introduceti mesajul criptat: ");
+			scanf_s(" %s", &mesaj, sizeof(mesaj));
+			printf("Introduceti cheia de deplasare: ");
+			scanf_s("%d", &key);
+			for (i = 0; i < strlen(mesaj); ++i)
+			{
+				if (isupper(mesaj[i]))
+				{
+					if (mesaj[i] + key < 'A') {
+						printf("%c", mesaj[i] - key);
+					}
+					else if (mesaj[i] + key > 'A') {
+						printf("%c", (mesaj[i] - key) + 26);
+					}
+				}
+				else {
+					if (isdigit(mesaj[i]))
+					{
+						if ((mesaj[i] - key) >= '0')
+						{
+							printf("%c", mesaj[i] - key);
+						}
+						else {
+							printf("%c", (mesaj[i] - key) + 10);
+						}
+					}
+					else if (mesaj[i] + key > 'a') {
+						printf("%c", mesaj[i] - key);
+					}
+					else if (mesaj[i] + key < 'A') {
+						printf("%c", mesaj[i] - key);
+					}
+					else if (mesaj[i] + key < 'a') {
+						printf("%c", (mesaj[i] - key) + 26);
+					}
+
+				}
+			}
+			break;
+		default:
+			printf("Va rugam alegeti una dintre optiunile afisate");
+			break;
 		}
 	}
 	return 0;
